9b95afe056f092841437392d80e05f2cf7fabb71
Vladimir <vladimir.rdguez@gmail.com>
2022-11-01 14:30:29 -0400

    Add initial profits

---
+++ generate_profit_example.py
@@ -0,0 +1,14 @@
+from store.models import Transaction, Order, Stock, Profit
+
+sell_orders = Order.objects.filter(type='sell')
+
+for order in sell_orders:
+	sells = Transaction.objects.filter(order=order)
+	for sell in sells:
+		stock = Stock.objects.filter(product=sell.product)[0]
+		gain = sell.quantity*(sell.price*(1-sell.product.sell_tax/100)-stock.cost)
+		profit = Profit.objects.create(product=sell.product,
+									  quantity=sell.quantity,
+									  created_date=order.created_date,
+									  profit=gain)
+		profit.save()
