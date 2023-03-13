859ad5ba8a87322a34f3dbeec4ee5bf2162b8029
Vladimir <vladimir.rdguez@gmail.com>
2023-03-13 10:30:07 -0400

    Weekly report

--- gestor/views.py
+++ gestor/views.py
@@ -1,7 +1,5 @@
-import datetime
+from datetime import datetime, timedelta
 from typing import List
-from collections import OrderedDict
-from operator import itemgetter, attrgetter
 from django.shortcuts import (
     render,
     redirect,
@@ -19,7 +17,13 @@ from services.views import (
 
 
 def getOrderBalance(order: Order, products: dict):
-    computeOrderAmount(order)
+    (transactions, services, expenses) = computeOrderAmount(order)
+
+    # compute labor income
+    order.labor = -order.discount
+    for service in services:
+        order.labor += service.amount
+
     # Consumables and parts
     transactions: List[ProductTransaction] = ProductTransaction.objects.filter(
         order=order)
@@ -29,6 +33,7 @@ def getOrderBalance(order: Order, products: dict):
         product = trans.product
         if product in products.keys():
             products[product]['quantity'] += trans.quantity
+            products[product]['cost'] += trans.cost
             products[product]['profit'] += computeTransactionProfit(
                 trans,
                 procedure="profit")
@@ -38,10 +43,11 @@ def getOrderBalance(order: Order, products: dict):
                 'name': product.name,
                 'unit': product.unit,
                 'quantity': trans.quantity,
+                'cost': trans.cost,
                 'profit': computeTransactionProfit(trans, procedure="profit")
             })
         if product.type == 'part':
-            parts_cost += trans.getMinCost()
+            parts_cost += trans.cost
         if product.type == 'consumable':
             consumable_expenses += trans.cost
     # Third party expenses
@@ -58,6 +64,7 @@ def getOrderBalance(order: Order, products: dict):
                  - order.parts
                  - order.consumable
                  - order.third_party
+                 - order.discount
                  )
     order.amount += order.tax
 
@@ -92,6 +99,31 @@ def report(request, year, month):
 
 
 @login_required
+def weekly_report(request, date=None):
+
+    (start_date, end_date, previousWeek, nextWeek) = getWeek(date)
+
+    orders = Order.objects.filter(
+        status='complete',
+        type='sell',
+        terminated_date__gt=start_date,
+        terminated_date__lte=end_date).order_by(
+        '-terminated_date').exclude(
+        associated__membership=True).exclude(
+        company__membership=True)
+
+    costs = Cost.objects.filter(date__gt=start_date,
+                                date__lte=end_date).order_by("-date")
+
+    context = computeReport(orders, costs)
+    context.setdefault('start_date', start_date)
+    context.setdefault('end_date', end_date)
+    context.setdefault('previousWeek', previousWeek.strftime("%m%d%Y"))
+    context.setdefault('nextWeek', nextWeek.strftime("%m%d%Y"))
+    return render(request, 'weekly.html', context)
+
+
+@login_required
 def dashboard(request):
     if (request.user.profile_user.role == 2):  # Mechanic
         return redirect('list-service-order')
@@ -106,27 +138,46 @@ def computeReport(orders, costs):
     third_party = 0
     net = 0
     tax = 0
+    discount = 0
     products = {}
+    orders.labor = 0
     for order in orders:
         getOrderBalance(order, products)
+        orders.labor += order.labor
         parts += order.parts
         consumable += order.consumable
         gross += order.amount
         third_party += order.third_party
         net += order.net
         tax += order.tax
+        discount += order.discount
+
     total = {
         'parts': parts,
         'consumable': consumable,
         'gross': gross,
         'third_party': third_party,
         'net': net,
+        'discount': discount,
         'tax': tax,
     }
     # Costs
     costs.total = 0
+    cats = {}
     for cost in costs:
+        if cost.category not in cats.keys():
+            cats.setdefault(cost.category, [cost.amount, 1])
+        else:
+            cats[cost.category][0] += cost.amount
+            cats[cost.category][1] += 1
         costs.total += cost.amount
+    # Sort by amount
+    sorted_cats = sorted(
+        cats, key=lambda cat: cats[cat][0], reverse=True)
+
+    for cat in sorted_cats:
+        cat.amount = cats[cat][0]
+        cat.items = cats[cat][1]
 
     # Product incomes
     parts_profit = 0
@@ -144,11 +195,19 @@ def computeReport(orders, costs):
     for product in sorted_products:
         product.profit = products[product]['profit']
         product.quantity = products[product]['quantity']
+        product.cost = products[product]['cost']
+        if products[product]['cost'] != 0:
+            product.efficiency = int(
+                100*products[product]['profit']/products[product]['cost'])
+        else:
+            product.efficiency = None
 
     return {
         'orders': orders,
         'total': total,
         'costs': costs,
+        'cost_cats': sorted_cats,
+        'profit': total['net'] - costs.total,
         'products': sorted_products,
         'parts_profit': parts_profit,
         'consumables_profit': consumables_profit,
@@ -167,17 +226,29 @@ def computeTransactionProfit(transaction: ProductTransaction, procedure="min"):
                 - transaction.cost)
 
 
+def getWeek(dt=None):
+    if dt is None:
+        dt = datetime.now()
+    else:
+        dt = datetime.strptime(dt, "%m%d%Y")
+    start = dt - timedelta(days=dt.weekday())
+    end = start + timedelta(days=7)
+    previousWeek = dt - timedelta(days=7)
+    nextWeek = dt + timedelta(days=7)
+    return (start.date(), end.date(), previousWeek, nextWeek)
+
+
 def getMonthYear(month=None, year=None):
     # Current
     if month is None:
-        currentMonth = datetime.datetime.now().month
+        currentMonth = datetime.now().month
     else:
         month = int(month)
         if month > 12 or month < 0:
             raise ValueError(F'Wrong month value: {month}!')
         currentMonth = month
     if year is None:
-        currentYear = datetime.datetime.now().year
+        currentYear = datetime.now().year
     else:
         year = int(year)
         currentYear = year
