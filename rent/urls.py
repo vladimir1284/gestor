69f6a99a4bd93f3903c06ed5317421e93dcda7c9
vladimir1284 <vladimir.rdguez@gmail.com>
2023-06-26 12:54:34 -0400

    Working on trackers

--- rent/urls.py
+++ rent/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import vehicle
+from .views import vehicle, tracker
 
 
 urlpatterns = [
@@ -11,4 +11,14 @@ urlpatterns = [
     path('delete-trailer/<id>', vehicle.delete_trailer, name='delete-trailer'),
     path('detail-trailer/<id>', vehicle.detail_trailer, name='detail-trailer'),
     path('select-towit', vehicle.select_towit, name='select-towit'),
+    # -------------------- Tracker ----------------------------
+    path('create-tracker/<int:trailer_id>',
+         tracker.TrackerCreateView.as_view(), name='create-tracker'),
+    path('update-tracker/<slug:pk>',
+         tracker.TrackerUpdateView.as_view(), name='update-tracker'),
+    path('delete-tracker/<int:id>',  tracker.delete_tracker, name='delete-tracker'),
+    path('detail-tracker/<int:id>',  tracker.tracker_detail, name='detail-tracker'),
+    path('trackers-map/',  tracker.trackers, name='trackers-map'),
+    path('trackers/',  tracker.trackers_table, name='trackers-table'),
+    path('tracker-upload', tracker.tracker_upload, name='tracker-upload'),
 ]
