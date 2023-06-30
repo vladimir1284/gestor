4006ce3105d489ea7eb9f3ef01055e740bfcb0ac
vladimir1284 <vladimir.rdguez@gmail.com>
2023-06-30 19:50:44 -0400

    Fixing imports

--- rent/forms/vehicle.py
+++ rent/forms/vehicle.py
@@ -2,7 +2,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from .models import (
+from rent.models.vehicle import (
     Trailer,
 )
 from utils.forms import (
