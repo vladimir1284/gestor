4106f7539ec2733f2b2ca8780b17893ccdde0d63
vladimir1284 <vladimir.rdguez@gmail.com>
2023-08-14 10:23:23 -0400

    backing up wordpress store

--- backups.sh
+++ backups.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# --------------------- Backup gestor ---------------------------
 # Change to the proyect directory
 cd /home/vladimir/gestor/
 
 # make a local archive
 tar -zcvf backups/db.backup-$(date +%F).tar.gz db.sqlite3
+tar -zcvf backups/media.backup-$(date +%F).tar.gz media
 
 # backup to drive
 sleep 1
@@ -15,6 +17,20 @@ rclone sync backups remote:towit/backups/db
 sleep 1
 find backups/ -type f -name "*.tar.gz" -mtime +14 -exec rm {} \;
 
-# Save all the images and documents to drive
+# --------------------- Backup wordpress ---------------------------
+# Change to the proyect directory
+cd /home/vladimir/
+
+# Export database
+wp db export puntoino.sql
+
+tar -zcvf backups/db.backup-$(date +%F).tar.gz puntoino.sql
+tar -zcvf backups/files.backup-$(date +%F).tar.gz puntoino
+
+# backup to drive
 sleep 1
-rclone copy media remote:towit/backups/media
+rclone sync backups remote:wp
+
+# delete archives older than 3 days from disk
+sleep 1
+find backups/ -type f -name "*.tar.gz" -mtime +3 -exec rm {} \;
