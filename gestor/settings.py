f2aee690b6908416ea445e481431032086df30da
vladimir1284 <vladimir.rdguez@gmail.com>
2023-12-27 10:37:22 -0500

    Including all subdomains

--- gestor/settings.py
+++ gestor/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'k2z9po4i#n+1p(^ny1el2c!om(^-l+_%&ob0azk0-ike*-)81e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["towit.pythonanywhere.com", 'towithouston.com', 'ergolease.towithouston.com',
+ALLOWED_HOSTS = ["towit.pythonanywhere.com", '.towithouston.com',
                  "localhost", '127.0.0.1', 'testserver', 'www.tacocars.com']
 
 
