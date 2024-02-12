4a345bc5b7d43d8d1c7f8fc1a861627cc57e7540
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-02-12 18:36:34 -0500

    formularios para los clientes

---
+++ rent/forms/lessee_contact.py
@@ -0,0 +1,15 @@
+from django import forms
+from rent.views.client import Associated
+
+
+class LesseeContactForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["name"].required = True
+        self.fields["phone_number"].required = True
+        self.fields["language"].required = True
+        self.fields["email"].required = False
+
+    class Meta:
+        model = Associated
+        fields = ("name", "phone_number", "language", "email")
