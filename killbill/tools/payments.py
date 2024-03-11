106c83be9073fa8b19aeb68c8a7bac94ec7c85f9
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-03-11 11:03:30 -0400

    killbill

--- killbill/tools/payments.py
+++ killbill/tools/payments.py
@@ -1,36 +1,65 @@
 from killbill.client import get_client
 from openapi_client import AccountApi
+from openapi_client import ApiClient
 from openapi_client import InvoiceApi
 from openapi_client import InvoicePayment
 from openapi_client import PaymentApi
 from rent.models.lease import Payment
 
 
-def _make_payment(accountApi: AccountApi, api: PaymentApi, pay: Payment):
+def _make_payment(apiClient: ApiClient, pay: Payment):
+    accountApi = AccountApi(apiClient)
+    PaymentApi(apiClient)
+    invoiceApi = InvoiceApi(apiClient)
+
     client = pay.client
     account = accountApi.get_account_by_key(external_key=str(client.id))
     if account is None or account.account_id is None:
         return
 
     invoices = accountApi.get_invoices_for_account(
-        account_id=account.account_id)
+        account_id=account.account_id,
+        unpaid_invoices_only=True,
+        # include_invoice_components=True,
+    )
+    print("###################################")
     print(invoices)
+    if len(invoices) == 0:
+        return
+
+    invoice = invoiceApi.get_invoice(
+        invoice_id=invoices[-1].invoice_id,
+    )
+    print("===================================")
+    print(invoice)
 
     client = get_client()
     invoiceApi = InvoiceApi(client)
-    invoiceApi.create_instant_payment(
+    invoice_pay = invoiceApi.create_instant_payment_without_preload_content(
         x_killbill_created_by="admin",
-        invoice_id=invoices[0].invoice_id,
+        invoice_id=invoices[-1].invoice_id,
+        external_payment=True,
         invoice_payment=InvoicePayment(
             accountId=account.account_id,
+            authAmount=float(pay.amount),
             capturedAmount=float(pay.amount),
+            purchasedAmount=float(pay.amount),
         ),
     )
+    print("###################################")
+    print(invoice_pay)
+
+    # paymentApi.capture_authorization(
+    #     payment_id=invoice_pay.payment_id,
+    #     x_killbill_created_by="admin",
+    #     payment_transaction=PaymentTransaction(
+    #         paymentId=invoice_pay.payment_id,
+    #         amount=invoice_pay.captured_amount,
+    #         processedAmount=invoice_pay.captured_amount,
+    #     ),
+    # )
 
 
 def make_payment(pay: Payment):
     client = get_client()
-    account_api = AccountApi(client)
-    pay_api = PaymentApi(client)
-
-    _make_payment(account_api, pay_api, pay)
+    _make_payment(client, pay)
--- killbill/tools/sync_accounts.py
+++ killbill/tools/sync_accounts.py
@@ -1,6 +1,9 @@
-from killbill.tools.get_lessee import Associated, get_lessee
-from openapi_client import Account, AccountApi
+from killbill.tools.get_lessee import Associated
+from killbill.tools.get_lessee import get_lessee
+from openapi_client import Account
+from openapi_client import AccountApi
 from openapi_client.api_client import NotFoundException
+from rent.models.lease import LesseeData
 
 
 def sync_account(account_api: AccountApi):
@@ -9,7 +12,10 @@ def sync_account(account_api: AccountApi):
     for lessee in lessees:
         try:
             account_id = str(lessee.id)
-            account_api.get_account_by_key(external_key=account_id)
+            account = account_api.get_account_by_key(external_key=account_id)
+            if account is None or account.account_id is None:
+                continue
+            update_account(account_api, account, lessee)
         except NotFoundException:
             sync_lessee(account_api, lessee)
         except Exception as e:
@@ -17,9 +23,39 @@ def sync_account(account_api: AccountApi):
             print(e)
 
 
+def update_account(account_api: AccountApi, account: Account, lessee: Associated):
+    print(
+        f"###### Updating account {account.account_id} -> {lessee.id}: {lessee.name} ######"
+    )
+    lesseeData = LesseeData.objects.filter(associated=lessee).first()
+    account.name = lessee.name
+    account.first_name_length = len(lessee.name)
+    account.email = lessee.email
+    account.city = lessee.city
+    account.state = lessee.state
+    account.phone = lessee.phone_number.__str__()
+    account.currency = "USD"
+    account.time_zone = "Etc/UTC"
+    account.country = "US"
+    account.locale = "en_US"
+    account.address1 = (
+        lesseeData.client_address if lesseeData is not None else "No Address"
+    )
+    account.address2 = ""
+    account.postal_code = "123456"
+    account.company = "TOWIT"
+
+    account_api.update_account_without_preload_content(
+        account_id=account.account_id,
+        x_killbill_created_by="admin",
+        account=account,
+    )
+
+
 def sync_lessee(account_api: AccountApi, lessee: Associated):
     account_id = str(lessee.id)
     print(f"###### Creating account {account_id}: {lessee.name} ######")
+    lesseeData = LesseeData.objects.filter(associated=lessee).first()
     account = Account(
         name=lessee.name,
         firstNameLength=len(lessee.name),
@@ -32,6 +68,10 @@ def sync_lessee(account_api: AccountApi, lessee: Associated):
         timeZone="Etc/UTC",
         country="US",
         locale="en_US",
+        address1=lesseeData.client_address if lesseeData is not None else "No Address",
+        address2="",
+        postalCode="123456",
+        company="TOWIT",
     )
     try:
         account_api.create_account("admin", account)
