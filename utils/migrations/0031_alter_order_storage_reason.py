7de2f35bb49100a6be8468215a0243fb13604cf7
Julio Guillermo Mayo Vidal <julioguillermo0802@gmail.com>
2024-03-27 17:07:10 -0400

    migra problem

--- utils/migrations/0031_alter_order_storage_reason.py
+++ utils/migrations/0031_alter_order_storage_reason.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('utils', '0030_order_storage_reason_ordertrace_reason_and_more'),
+        ('utils', '0031_alter_order_storage_reason'),
     ]
 
     operations = [
