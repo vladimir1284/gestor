e304ee145e50573f0f3eb8dbc85cb513a901a96a
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-04-08 15:54:43 -0400

    model multiselect to checkbox

--- rbac/forms/user_form.py
+++ rbac/forms/user_form.py
@@ -4,7 +4,9 @@ from crispy_forms.helper import Layout
 from crispy_forms.helper import mark_safe
 from crispy_forms.layout import Div
 from crispy_forms.layout import Field
+from crispy_forms.layout import HTML
 from django import forms
+from django.contrib.auth.models import Group
 from django.contrib.auth.models import User
 
 
@@ -20,7 +22,14 @@ class UserForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["groups"].label = "Roles"
+        self.fields["groups"] = forms.ModelMultipleChoiceField(
+            queryset=Group.objects.all(),
+            widget=forms.CheckboxSelectMultiple,
+            required=False,
+            label="",
+        )
+        # self.fields["groups"].label = "Roles"
+        # self.fields["groups"].widget = forms.CheckboxSelectMultiple()
         self.helper = FormHelper()
         self.helper.form_tag = False  # Don't render form tag
         self.helper.disable_csrf = True  # Don't render CSRF token
@@ -53,14 +62,45 @@ class UserForm(forms.ModelForm):
                 ),
                 css_class="row mb-3",
             ),
+            # Div(
+            #     Field(
+            #         "groups",
+            #         # PrependedText(
+            #         #     "groups",
+            #         #     mark_safe('<i class="bx bx-cog"></i>'),
+            #         # )
+            #     ),
+            #     css_class="row mb-3",
+            # ),
             Div(
-                Field(
-                    PrependedText(
-                        "groups",
-                        mark_safe('<i class="bx bx-cog"></i>'),
-                    )
+                Div(
+                    HTML(
+                        """
+                            <h2 class="accordion-header d-flex">
+                            <input type="checkbox" class="mark-all checkboxinput">
+                            <button
+                            data-bs-target="#groups"
+                            class="accordion-button collapsed text-primary"
+                            type="button"
+                            data-bs-toggle="collapse">
+                                Roles
+                            </button>
+                            </h2>
+                        """
+                    ),
+                    Div(
+                        Div(
+                            Field("groups"),
+                            css_class="accordion-body",
+                        ),
+                        css_id="groups",
+                        css_class="accordion-collapse collapse",
+                    ),
+                    css_id="groups-accordion",
+                    css_class="accordion-item border-bottom",
                 ),
-                css_class="row mb-3",
+                css_id="others-accordion",
+                css_class="accordion mb-4 border rounded",
             ),
             # ButtonHolder(
             #     Submit(
