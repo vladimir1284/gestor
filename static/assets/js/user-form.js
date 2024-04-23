81d89f57d4e4a93e69b1b8a6a26e94b2ad3e7b54
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-04-23 18:34:26 -0400

    search bar to user and role -form

--- static/assets/js/user-form.js
+++ static/assets/js/user-form.js
@@ -1,46 +1,46 @@
 const Roles = window.jroles;
 
 const roles = document.querySelectorAll(
-    '#div_id_groups input[type="checkbox"]',
+  '#div_id_groups input[type="checkbox"]',
 );
 const menu = document.querySelectorAll('#menu_perms input[type="checkbox"]');
 const urls = document.querySelectorAll('#urls_perms input[type="checkbox"]');
 const dash = document.querySelectorAll(
-    '#dashboard_card_perms input[type="checkbox"]',
+  '#dashboard_card_perms input[type="checkbox"]',
 );
 const extra = document.querySelectorAll('#extra_perms input[type="checkbox"]');
 
-const accordionCollapse = document.querySelectorAll(
-    ".accordion-collapse.collapse",
-);
-const accordionButton = document.querySelectorAll(
-    ".accordion-button.collapsed",
-);
-
-accordionCollapse.forEach((ac) => {
-    ac.setAttribute(":class", '{show: $store.search.search != ""}');
-});
-accordionButton.forEach((ac) => {
-    ac.setAttribute(":class", '{collapsed: $store.search.search == ""}');
-});
+// const accordionCollapse = document.querySelectorAll(
+//     ".accordion-collapse.collapse",
+// );
+// const accordionButton = document.querySelectorAll(
+//     ".accordion-button.collapsed",
+// );
+//
+// accordionCollapse.forEach((ac) => {
+//     ac.setAttribute(":class", '{show: $store.search.search != ""}');
+// });
+// accordionButton.forEach((ac) => {
+//     ac.setAttribute(":class", '{collapsed: $store.search.search == ""}');
+// });
 
 const refs = [];
 
 roles.forEach((r) => {
-    r.setAttribute("x-model", `model.role_${r.value}`);
-    r.classList.add("checkboxinput");
-    r.parentNode.setAttribute("x-show", "() => match($el)");
+  r.setAttribute("x-model", `model.role_${r.value}`);
+  r.classList.add("checkboxinput");
+  // r.parentNode.setAttribute("x-show", "() => match($el)");
 });
 
 function initPerms(perms, model) {
-    perms.forEach((e) => {
-        const ref = `perm.${e.dataset["perm"]}`;
-        const xmodel = `${model}.${e.dataset["perm"]}`;
-        refs.push(ref);
-        e.setAttribute("x-ref", ref);
-        e.setAttribute("x-model", xmodel);
-        e.parentNode.setAttribute("x-show", "() => match($el)");
-    });
+  perms.forEach((e) => {
+    const ref = `perm.${e.dataset["perm"]}`;
+    const xmodel = `${model}.${e.dataset["perm"]}`;
+    refs.push(ref);
+    e.setAttribute("x-ref", ref);
+    e.setAttribute("x-model", xmodel);
+    // e.parentNode.setAttribute("x-show", "() => match($el)");
+  });
 }
 
 initPerms(menu, "menu");
@@ -49,198 +49,198 @@ initPerms(dash, "dash");
 initPerms(extra, "extra");
 
 function getARPerms(m) {
-    const perms = [];
-    Roles.forEach((r) => {
-        if (!m[`role_${r.id}`]) return;
+  const perms = [];
+  Roles.forEach((r) => {
+    if (!m[`role_${r.id}`]) return;
 
-        r.permissions.forEach((p) => {
-            if (perms.indexOf(p) == -1) {
-                perms.push(p);
-            }
-        });
+    r.permissions.forEach((p) => {
+      if (perms.indexOf(p) == -1) {
+        perms.push(p);
+      }
     });
-    return perms;
+  });
+  return perms;
 }
 
 function getState(elements) {
-    let mark = false;
-    let unmark = false;
-    for (let k in elements) {
-        if (elements[k]) {
-            mark = true;
-        } else {
-            unmark = true;
-        }
-    }
-
-    if (mark && !unmark) {
-        return 1;
-    } else if (!mark && unmark) {
-        return 0;
+  let mark = false;
+  let unmark = false;
+  for (let k in elements) {
+    if (elements[k]) {
+      mark = true;
     } else {
-        return -1;
+      unmark = true;
     }
+  }
+
+  if (mark && !unmark) {
+    return 1;
+  } else if (!mark && unmark) {
+    return 0;
+  } else {
+    return -1;
+  }
 }
 
 document.addEventListener("alpine:init", () => {
-    Alpine.data("UserRolesPerms", () => {
-        return {
-            model: {},
-            menu: {},
-            urls: {},
-            dash: {},
-            extra: {},
+  Alpine.data("UserRolesPerms", () => {
+    return {
+      model: {},
+      menu: {},
+      urls: {},
+      dash: {},
+      extra: {},
 
-            init() {
-                this.$watch("model", (m) => {
-                    this.updateAllRoles(m);
-                    this.updatePerms(m);
-                });
-                this.$watch("menu", (m) => {
-                    this.updateMenuAll(m);
-                });
-                this.$watch("urls", (m) => {
-                    this.updateUrlsAll(m);
-                });
-                this.$watch("dash", (m) => {
-                    this.updateDashAll(m);
-                });
-                this.$watch("extra", (m) => {
-                    this.updateExtraAll(m);
-                });
-                roles.forEach((r) => {
-                    this.model[`role_${r.value}`] = r.checked;
-                });
-                menu.forEach((p) => {
-                    this.menu[p.dataset["perm"]] = p.checked;
-                });
-                urls.forEach((p) => {
-                    this.urls[p.dataset["perm"]] = p.checked;
-                });
-                dash.forEach((p) => {
-                    this.dash[p.dataset["perm"]] = p.checked;
-                });
-                extra.forEach((p) => {
-                    this.extra[p.dataset["perm"]] = p.checked;
-                });
-            },
+      init() {
+        this.$watch("model", (m) => {
+          this.updateAllRoles(m);
+          this.updatePerms(m);
+        });
+        this.$watch("menu", (m) => {
+          this.updateMenuAll(m);
+        });
+        this.$watch("urls", (m) => {
+          this.updateUrlsAll(m);
+        });
+        this.$watch("dash", (m) => {
+          this.updateDashAll(m);
+        });
+        this.$watch("extra", (m) => {
+          this.updateExtraAll(m);
+        });
+        roles.forEach((r) => {
+          this.model[`role_${r.value}`] = r.checked;
+        });
+        menu.forEach((p) => {
+          this.menu[p.dataset["perm"]] = p.checked;
+        });
+        urls.forEach((p) => {
+          this.urls[p.dataset["perm"]] = p.checked;
+        });
+        dash.forEach((p) => {
+          this.dash[p.dataset["perm"]] = p.checked;
+        });
+        extra.forEach((p) => {
+          this.extra[p.dataset["perm"]] = p.checked;
+        });
+      },
 
-            updatePerms(m) {
-                const perms = getARPerms(m);
-                let menu = false;
-                let urls = false;
-                let dash = false;
-                let extra = false;
-                refs.forEach((r) => {
-                    const e = this.$refs[r];
-                    const p = e.dataset["perm"];
-                    if (perms.indexOf(p) != -1) {
-                        e.disabled = true;
-                        e.parentNode.classList.add("disabled");
-                    } else {
-                        if (p.startsWith("menu___")) {
-                            menu = true;
-                        }
-                        if (p.startsWith("urls___")) {
-                            urls = true;
-                        }
-                        if (p.startsWith("dashboard_card___")) {
-                            dash = true;
-                        }
-                        if (p.startsWith("extra_perm___")) {
-                            extra = true;
-                        }
-                        e.disabled = false;
-                        e.parentNode.classList.remove("disabled");
-                    }
-                });
+      updatePerms(m) {
+        const perms = getARPerms(m);
+        let menu = false;
+        let urls = false;
+        let dash = false;
+        let extra = false;
+        refs.forEach((r) => {
+          const e = this.$refs[r];
+          const p = e.dataset["perm"];
+          if (perms.indexOf(p) != -1) {
+            e.disabled = true;
+            e.parentNode.classList.add("disabled");
+          } else {
+            if (p.startsWith("menu___")) {
+              menu = true;
+            }
+            if (p.startsWith("urls___")) {
+              urls = true;
+            }
+            if (p.startsWith("dashboard_card___")) {
+              dash = true;
+            }
+            if (p.startsWith("extra_perm___")) {
+              extra = true;
+            }
+            e.disabled = false;
+            e.parentNode.classList.remove("disabled");
+          }
+        });
 
-                this.$refs.menu_all.disabled = !menu;
-                this.$refs.urls_all.disabled = !urls;
-                this.$refs.dash_all.disabled = !dash;
-                this.$refs.extra_all.disabled = !extra;
-            },
+        this.$refs.menu_all.disabled = !menu;
+        this.$refs.urls_all.disabled = !urls;
+        this.$refs.dash_all.disabled = !dash;
+        this.$refs.extra_all.disabled = !extra;
+      },
 
-            setStateOf(input, elements) {
-                const state = getState(elements);
-                if (state == 1) {
-                    input.checked = true;
-                    input.indeterminate = false;
-                } else if (state == 0) {
-                    input.checked = false;
-                    input.indeterminate = false;
-                } else {
-                    input.checked = true;
-                    input.indeterminate = true;
-                }
-            },
+      setStateOf(input, elements) {
+        const state = getState(elements);
+        if (state == 1) {
+          input.checked = true;
+          input.indeterminate = false;
+        } else if (state == 0) {
+          input.checked = false;
+          input.indeterminate = false;
+        } else {
+          input.checked = true;
+          input.indeterminate = true;
+        }
+      },
 
-            updateAllRoles(m) {
-                this.setStateOf(this.$refs.all_roles, m);
-            },
+      updateAllRoles(m) {
+        this.setStateOf(this.$refs.all_roles, m);
+      },
 
-            updateMenuAll(m) {
-                this.setStateOf(this.$refs.menu_all, m);
-            },
+      updateMenuAll(m) {
+        this.setStateOf(this.$refs.menu_all, m);
+      },
 
-            updateUrlsAll(m) {
-                this.setStateOf(this.$refs.urls_all, m);
-            },
+      updateUrlsAll(m) {
+        this.setStateOf(this.$refs.urls_all, m);
+      },
 
-            updateDashAll(m) {
-                this.setStateOf(this.$refs.dash_all, m);
-            },
+      updateDashAll(m) {
+        this.setStateOf(this.$refs.dash_all, m);
+      },
 
-            updateExtraAll(m) {
-                this.setStateOf(this.$refs.extra_all, m);
-            },
+      updateExtraAll(m) {
+        this.setStateOf(this.$refs.extra_all, m);
+      },
 
-            toggleAllRoles() {
-                const state = this.$el.checked;
-                for (let k in this.model) {
-                    this.model[k] = state;
-                }
-            },
+      toggleAllRoles() {
+        const state = this.$el.checked;
+        for (let k in this.model) {
+          this.model[k] = state;
+        }
+      },
 
-            toggleMenuAll() {
-                const state = this.$el.checked;
-                for (let k in this.menu) {
-                    this.menu[k] = state;
-                }
-            },
+      toggleMenuAll() {
+        const state = this.$el.checked;
+        for (let k in this.menu) {
+          this.menu[k] = state;
+        }
+      },
 
-            toggleUrlsAll() {
-                const state = this.$el.checked;
-                for (let k in this.urls) {
-                    this.urls[k] = state;
-                }
-            },
+      toggleUrlsAll() {
+        const state = this.$el.checked;
+        for (let k in this.urls) {
+          this.urls[k] = state;
+        }
+      },
 
-            toggleDashAll() {
-                const state = this.$el.checked;
-                for (let k in this.dash) {
-                    this.dash[k] = state;
-                }
-            },
+      toggleDashAll() {
+        const state = this.$el.checked;
+        for (let k in this.dash) {
+          this.dash[k] = state;
+        }
+      },
 
-            toggleExtraAll() {
-                const state = this.$el.checked;
-                for (let k in this.extra) {
-                    this.extra[k] = state;
-                }
-            },
+      toggleExtraAll() {
+        const state = this.$el.checked;
+        for (let k in this.extra) {
+          this.extra[k] = state;
+        }
+      },
 
-            match(element) {
-                const etext = element.innerText.toLowerCase();
-                const text = Alpine.store("search").search;
-                const fields = text.toLowerCase().split(" ");
-                for (let f of fields) {
-                    if (etext.indexOf(f) == -1) {
-                        return false;
-                    }
-                }
-                return true;
-            },
-        };
-    });
+      // match(element) {
+      //     const etext = element.innerText.toLowerCase();
+      //     const text = Alpine.store("search").search;
+      //     const fields = text.toLowerCase().split(" ");
+      //     for (let f of fields) {
+      //         if (etext.indexOf(f) == -1) {
+      //             return false;
+      //         }
+      //     }
+      //     return true;
+      // },
+    };
+  });
 });
