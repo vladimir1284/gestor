53b759a9ae7a83a17eb1cd8a46ad875e808c892b
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-04-25 17:38:05 -0400

    template versions system: contract durations...

--- rent/tools/get_conditions.py
+++ rent/tools/get_conditions.py
@@ -1,3 +1,5 @@
+from dateutil.relativedelta import relativedelta
+
 from rent.models.lease import Contract
 from rent.tools.init_conditions import LANG_EN
 from rent.tools.init_conditions import MODULE
@@ -8,6 +10,19 @@ from template_admin.models.template_version import TemplateVersion
 
 def get_conditions(ctx):
     contract: Contract = ctx["contract"]
+
+    # Lessee Initials
+    client_initials = ""
+    client_name = contract.lessee.name
+    for n in client_name.split(" "):
+        if n != "":
+            client_initials += n[0].upper()
+    ctx["lessee_initials"] = client_initials
+
+    delta = relativedelta(months=contract.contract_term)
+
+    contract.end_date = contract.effective_date + delta
+
     templ = TEMPLATE_RENT if contract.contract_type == "rent" else TEMPLATE_LTO
     version = contract.template_version
 
@@ -23,6 +38,7 @@ def get_conditions(ctx):
 
     replaces = {
         "@@signatures_and_date@@": '{% include "rent/contract/signatures_and_date.html" %}',
+        "@@signatures_and_date_v2@@": '{% include "rent/contract/signatures_and_date_v2.html" %}',
         "@@trailer_report@@": '{% include "rent/contract/trailer_condition_report.html" %}',
         "@@lessee_signature@@": '<img class="signature" src="{{ signature_lessee.img.url }}">',
     }
