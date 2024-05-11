8b2f6f5f8b3b8dae1a03f3429a066fefd0bf13d2
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-05-11 15:08:16 -0400

    use client on service order too

--- services/views/order_flow/create_order_contract.py
+++ services/views/order_flow/create_order_contract.py
@@ -2,13 +2,34 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from rent.forms.lessee_contact import LesseeContactForm
 from services.models.preorder import PARTS_SALE
 from services.models.preorder import QUOTATION
 from services.views.order_flow.fast_orders import Preorder
+from users.models import Associated
 from users.views import addStateCity
+
+
+def on_exists(args: list, client: Associated):
+    preorder: Preorder | None = args[0] if len(args) > 0 else None
+    if preorder is None:
+        preorder = Preorder(
+            associated=client,
+            new_associated=True,
+            using_signature=True,
+        )
+    else:
+        preorder.associated = client
+        preorder.new_associated = True
+        preorder.using_signature = False
+
+    preorder.save()
+    if preorder.concept == QUOTATION or preorder.concept == PARTS_SALE:
+        return "fast-order-create", [preorder.id]
+    return "generate-service-order-contact-url", [preorder.id]
 
 
 @login_required
@@ -18,7 +39,15 @@ def create_order_contact(request, id=None):
     )
 
     if request.method == "POST":
-        form = LesseeContactForm(request.POST, request.FILES)
+        form = LesseeContactForm(
+            request.POST,
+            request.FILES,
+            use_client_url={
+                "url": "",
+                "args": [preorder],
+                "on_exists": on_exists,
+            },
+        )
         if form.is_valid():
             associated = form.save()
 
