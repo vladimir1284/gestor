ff6d6b7b7a0bbc377efbb5d2a60db7b8b26baffb
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-05-27 17:27:41 -0400

    Tabs for Actives, Completed and Pending Security devolutions

--- rent/tools/security_deposit_reports.py
+++ rent/tools/security_deposit_reports.py
@@ -4,36 +4,49 @@ from rent.tools.adjust_security_deposit import adjust_security_deposit
 
 
 def security_deposit_reports(ctx: dict, year: int, month: int):
-    completed = SecurityDepositDevolution.objects.filter(
-        returned_date__year=year,
-        returned_date__month=month,
-        returned=True,
-    )
+    completed = []
+    pendings = []
+    actives = []
 
     total_returned = 0.0
     total_income = 0.0
     total_active = 0.0
+    total_pending = 0.0
 
-    completed_contracts = []
-    for c in completed:
-        total_returned += c.amount
-        total_income += c.income
-        if c.contract.id not in completed_contracts:
-            completed_contracts.append(c.contract.id)
-
-    contracts = Contract.objects.exclude(
-        id__in=completed_contracts,
-    ).exclude(
-        stage__in=["ended", "gaarbage"],
-    )
-    actives = []
-    for c in contracts:
-        _, _, deposit = adjust_security_deposit(c.id)
-        actives.append(deposit)
-        total_active += deposit.total_deposited_amount
+    # For each active or completed contracts
+    contracts = Contract.objects.filter(stage__in=["active", "ended"])
+    for contract in contracts:
+        # Get contract SecurityDepositDevolution
+        _, _, deposit = adjust_security_deposit(contract.id)
+        # Already returned
+        if deposit.contract.stage == "ended" and deposit.returned:
+            # Sub conditional because it does not matter if the returned date does not match
+            # to be returned and we will NOT include it in the active list
+            if (
+                deposit.returned_date is not None
+                and deposit.returned_date.year == year
+                and deposit.returned_date.month == month
+            ):
+                completed.append(deposit)
+                total_returned += deposit.amount
+                total_income += deposit.income
+        # Will be returned
+        elif deposit.contract.stage == "ended" and deposit.refund_date is not None:
+            # Sub conditional because it does not matter if the future returned date does not match
+            # to be a pending return and we will NOT include it in the active list
+            if deposit.refund_date.year == year and deposit.refund_date.month == month:
+                pendings.append(deposit)
+                total_pending += deposit.total_deposited_amount
+        # is active
+        # Just include if it is not a returned or a pending
+        # Just include if contract stage is active, not ended
+        elif deposit.contract.stage == "active":
+            actives.append(deposit)
+            total_active += deposit.total_deposited_amount
 
     ctx["security_actives"] = actives
     ctx["security_completed"] = completed
+    ctx["security_pending"] = pendings
     ctx["security_total_active"] = total_active
     ctx["security_total_income"] = total_income
     ctx["security_total_returned"] = total_returned
