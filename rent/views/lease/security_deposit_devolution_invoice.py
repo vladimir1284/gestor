a44787ad454b90a97ad957bd39a884a173c39760
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-05-29 07:36:58 -0400

    fix on_hold card number total income of finish and colors

--- rent/views/lease/security_deposit_devolution_invoice.py
+++ rent/views/lease/security_deposit_devolution_invoice.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 from gestor.views.reports import get_object_or_404
 from rent.models.deposit_discount import DepositDiscount
@@ -18,5 +21,27 @@ def security_deposit_devolution_invoices(request: HttpRequest, id: int):
     ctx = {
         "devolution": dev,
         "discount": dis,
+        "pdf": True,
     }
-    return render(request, "rent/deposits/devolution_invoice.html", ctx)
+    html_string = render_to_string(
+        "rent/deposits/devolution_invoice.html",
+        ctx,
+    )
+    if settings.USE_WEASYPRINT:
+        from weasyprint import HTML
+
+        html = HTML(string=html_string, base_url=request.build_absolute_uri())
+        result = html.write_pdf()
+        if result is not None:
+            response = HttpResponse(content_type="application/pdf;")
+            response["Content-Disposition"] = "inline; filename=invoice_towit.pdf"
+            response["Content-Transfer-Encoding"] = "binary"
+            response.status_code = 200
+            response.write(result)
+            return response
+
+    return render(
+        request,
+        "rent/deposits/devolution_invoice.html",
+        ctx,
+    )
