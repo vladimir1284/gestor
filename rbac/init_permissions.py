23b0bfd274d078367c152c582691cc7f952447d7
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-06-03 13:37:20 -0400

    clean migrations and deps from django-googledrive-storage

--- rbac/init_permissions.py
+++ rbac/init_permissions.py
@@ -2,4 +2,7 @@ from rbac.tools.sync_permissions import sync_permissions
 
 
 def init_permissions():
-    sync_permissions()
+    try:
+        sync_permissions()
+    except Exception as e:
+        print(e)
