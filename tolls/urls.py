da27261079c85ad710679a53368c322e05465c57
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-06-07 10:13:35 -0400

    ended contract tolls creation and new toll trailer filter fix

--- tolls/urls.py
+++ tolls/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
-from .views import list_toll, create_toll, update_toll, delete_toll
+
+from .views import create_contract_toll
+from .views import create_toll
+from .views import delete_toll
+from .views import list_toll
+from .views import update_toll
 
 urlpatterns = [
-    path('list-toll/', list_toll, name='list-toll'),
-    path('list-toll/<id>/', list_toll, name='list-toll'),
-    path('create-toll/', create_toll, name='create-toll'),
-    path('create-toll/<int:plate>/', create_toll, name='create-toll'),
-    path('create-toll/<int:plate>/<int:contract>/', create_toll, name='create-toll'),
-    path('update-toll/<int:id>/', update_toll, name='update-toll'),
-    path('delete-toll/<int:id>/', delete_toll, name='delete-toll')
+    path("list-toll/", list_toll, name="list-toll"),
+    path("list-toll/<id>/", list_toll, name="list-toll"),
+    path("create-toll/", create_toll, name="create-toll"),
+    path("create-toll/<int:plate>/", create_toll, name="create-toll"),
+    path("create-toll/<int:plate>/<int:contract>/", create_toll, name="create-toll"),
+    path(
+        "create-contract-toll/<int:contract>/",
+        create_contract_toll,
+        name="create-contract-toll",
+    ),
+    path("update-toll/<int:id>/", update_toll, name="update-toll"),
+    path("delete-toll/<int:id>/", delete_toll, name="delete-toll"),
 ]
+
--- tolls/views.py
+++ tolls/views.py
@@ -1,71 +1,93 @@
-
-from django.shortcuts import render, redirect, get_object_or_404, reverse
-from django.contrib.auth.decorators import login_required
-from gestor.views.utils import getMonthYear
-from .models import TollDue
 from datetime import datetime
-from .forms import TollCreateForm, TollUpdateForm
 
-from rent.models.vehicle import TrailerPlates
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.shortcuts import reverse
+
+from .forms import TollCreateForm
+from .forms import TollUpdateForm
+from .models import TollDue
+from gestor.views.utils import getMonthYear
 from rent.models.lease import Contract
-
+from rent.models.vehicle import TrailerPlates
 
 
 @login_required
 def create_toll(request, plate=None, contract=None):
     if not plate:
-        context = {
-            'plates': TrailerPlates.objects.all()
-        }
-        return render(request, 'tolls/plates_list.html', context)
+        context = {"plates": TrailerPlates.objects.all()}
+        return render(request, "tolls/plates_list.html", context)
     elif not contract:
         _plate = TrailerPlates.objects.get(id=plate)
         context = {
-            'contracts': Contract.objects.all().filter(trailer=_plate.trailer),
-            'plate': _plate
+            "contracts": Contract.objects.all().filter(trailer=_plate.trailer),
+            "plate": _plate,
         }
-        return render(request, 'tolls/contracts_list.html', context)
+        return render(request, "tolls/contracts_list.html", context)
     _plate = TrailerPlates.objects.get(id=plate)
     _contract = Contract.objects.get(id=contract)
-    next_id = request.GET.get('next_id', None)
+    next_id = request.GET.get("next_id", None)
     form = TollCreateForm(plate=_plate, contract=_contract)
     if request.method == "POST":
-        form = TollCreateForm(request.POST, request.FILES, plate=_plate, contract=_contract)
+        form = TollCreateForm(
+            request.POST, request.FILES, plate=_plate, contract=_contract
+        )
         if form.is_valid():
             form.save()
             if next_id:
-                return redirect(reverse('list-toll')+f"{next_id}")
+                return redirect(reverse("list-toll") + f"{next_id}")
             else:
-                return redirect('list-toll')
+                return redirect("list-toll")
 
-    context = {
-        'form': form,
-        'title': "Create Toll"
-    }
+    context = {"form": form, "title": "Create Toll"}
 
-    return render(request, 'tolls/toll_create.html', context)
+    return render(request, "tolls/toll_create.html", context)
+
+
+@login_required
+def create_contract_toll(request, contract: int):
+    _contract = Contract.objects.get(id=contract)
+    _plate = TrailerPlates.objects.get(trailer=_contract.trailer)
+
+    if request.method == "POST":
+        form = TollCreateForm(
+            request.POST, request.FILES, plate=_plate, contract=_contract
+        )
+        if form.is_valid():
+            form.save()
+            return redirect("ended-contract-details", contract)
+    else:
+        form = TollCreateForm(plate=_plate, contract=_contract)
+
+    context = {"form": form, "title": "Create Contract Toll"}
+
+    return render(request, "tolls/toll_create.html", context)
+
 
 @login_required
 def list_toll(request, id=None):
 
-    year = request.GET.get('year', None)
-    month = request.GET.get('month', None)
+    year = request.GET.get("year", None)
+    month = request.GET.get("month", None)
 
+    (
+        (previousMonth, previousYear),
+        (currentMonth, currentYear),
+        (nextMonth, nextYear),
+    ) = getMonthYear(month, year)
 
-    ((previousMonth, previousYear),
-     (currentMonth, currentYear),
-     (nextMonth, nextYear)) = getMonthYear(month, year)
-
-    
     if not id:
         tolls = TollDue.objects.all().order_by("-created_date", "-id")
-        tolls = tolls.filter(created_date__year=currentYear,
-            created_date__month=currentMonth)
+        tolls = tolls.filter(
+            created_date__year=currentYear, created_date__month=currentMonth
+        )
     else:
         tolls = TollDue.objects.all().filter(contract=id).order_by("-created_date")
 
-    start_date = request.GET.get('start_date')
-    end_date = request.GET.get('end_date')
+    start_date = request.GET.get("start_date")
+    end_date = request.GET.get("end_date")
 
     if start_date is not None:
         tolls = tolls.filter(created_date__gte=start_date)
@@ -82,55 +104,54 @@ def list_toll(request, id=None):
         if _contract:
             contract = _contract[0]
 
-
     context = {
-        'previousMonth': previousMonth,
-        'currentMonth': currentMonth,
-        'nextMonth': nextMonth,
-        'thisMonth': datetime.now().month,
-        'previousYear': previousYear,
-        'currentYear': currentYear,
-        'nextYear': nextYear,
-        'thisYear': datetime.now().year,
-        'interval': 'monthly',
-        'tolls': tolls,
-        'id': id,
-        'contract': contract
+        "previousMonth": previousMonth,
+        "currentMonth": currentMonth,
+        "nextMonth": nextMonth,
+        "thisMonth": datetime.now().month,
+        "previousYear": previousYear,
+        "currentYear": currentYear,
+        "nextYear": nextYear,
+        "thisYear": datetime.now().year,
+        "interval": "monthly",
+        "tolls": tolls,
+        "id": id,
+        "contract": contract,
     }
-    return render(request, 'tolls/toll_list.html', context)
+    return render(request, "tolls/toll_list.html", context)
+
 
 @login_required
 def update_toll(request, id):
     toll = get_object_or_404(TollDue, id=id)
     form = TollUpdateForm(instance=toll, plate=toll.plate)
-    next_id = request.GET.get('next_id', None)
+    next_id = request.GET.get("next_id", None)
 
     print("toll", toll.contract.id, toll.contract.tolldue_set.all())
 
-    if request.method == 'POST':
-        form = TollUpdateForm(request.POST, request.FILES, instance=toll, plate=toll.plate)
+    if request.method == "POST":
+        form = TollUpdateForm(
+            request.POST, request.FILES, instance=toll, plate=toll.plate
+        )
         if form.is_valid():
             form.save()
             if next_id:
-                return redirect(reverse('list-toll')+f"{next_id}")
+                return redirect(reverse("list-toll") + f"{next_id}")
             else:
-                return redirect('list-toll')
+                return redirect("list-toll")
 
-    context = {
-        'title': 'Update toll',
-        'form': form,
-        'next_id': next_id
-    }
+    context = {"title": "Update toll", "form": form, "next_id": next_id}
 
-    return render(request, 'tolls/toll_create.html', context)
+    return render(request, "tolls/toll_create.html", context)
+
 
 @login_required
 def delete_toll(request, id):
     toll = get_object_or_404(TollDue, id=id)
-    next_id = request.GET.get('next_id', None)
+    next_id = request.GET.get("next_id", None)
     toll.delete()
     if next_id:
-        return redirect(reverse('list-toll')+f"{next_id}")
+        return redirect(reverse("list-toll") + f"{next_id}")
     else:
-        return redirect('list-toll')
-    
+        return redirect("list-toll")
+
