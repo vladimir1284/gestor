4baed281d5cffb8693aff587bf868f80472d2ede
julio guillermo mayo vidal <juliogmv@estudiantes.uci.cu>
2024-06-09 10:25:37 -0400

    generic client controller for parts and consumbales... consumables also working

---
+++ static/assets/js/orders/controllers/order_consumables.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+globalThis.initController(
+  "OrderConsumables",
+  globalThis.ProductConsumablesUrl,
+  globalThis.ProductTransactionConsumablesUrl,
+  globalThis.OrderID,
+  "alt+c",
+  "consumable",
+);
